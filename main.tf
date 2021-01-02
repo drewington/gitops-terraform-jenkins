a0e737634ee81a95a0557bc304ba3084a1391a77
Drew <50253349+drewington@users.noreply.github.com>
2021-01-02 02:41:41 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Terraform state will be stored in S3
 terraform {
   backend "s3" {
-    bucket = "terraform-bucket-alex"
+    bucket = "terraform-drew"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
